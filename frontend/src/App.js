7e8ca7b4f80f490ad40299174766a085abf8674f
shamini nirodya <nirodyapgs.22@uom.lk>
2024-09-24 19:13:15 +0530

    Changes h1 and New changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Go to changes</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          New Change <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
